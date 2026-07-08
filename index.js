62d97a20af2383fb05c599c50c31e8e4898299bd
kalebharrison <kalebrharrison@gmail.com>
2026-07-08 06:46:11 -0400

    chore: split json file store

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ import net from 'net';
 import { createBasePathHelpers, deriveBasePath } from './lib/base-path.js';
 import { createLruCache, createTtlCache } from './lib/cache.js';
 import { escapeHtmlAttr, injectBasePathHtml } from './lib/html-shell.js';
+import { loadFile, saveFile } from './lib/json-file-store.js';
 import { createRateLimiter } from './lib/rate-limit.js';
 
 let appVersion = 'v1.0.0';
@@ -550,52 +551,6 @@ const checkAndSendNotifications = async (config) => {
     }
 };
 
-
-// --- File I/O ---
-const fileLocks = new Map();
-
-const lockFile = async (path) => {
-    while (fileLocks.get(path)) {
-        await fileLocks.get(path);
-    }
-    let resolve;
-    const promise = new Promise(r => resolve = r);
-    fileLocks.set(path, promise);
-    return () => {
-        if (fileLocks.get(path) === promise) {
-            fileLocks.delete(path);
-        }
-        resolve();
-    };
-};
-
-const loadFile = async (path, defaultContent) => {
-    const unlock = await lockFile(path);
-    try {
-        const content = await fs.readFile(path, 'utf-8');
-        const data = JSON.parse(content);
-        return data === null ? defaultContent : data;
-    } catch (error) {
-        if (error.code === 'ENOENT') {
-            await fs.writeFile(path, JSON.stringify(defaultContent, null, 2));
-            return defaultContent;
-        }
-        throw error;
-    } finally {
-        unlock();
-    }
-};
-
-const saveFile = async (path, data) => {
-    const unlock = await lockFile(path);
-    try {
-        const tempPath = `${path}.tmp`;
-        await fs.writeFile(tempPath, JSON.stringify(data, null, 2));
-        await fs.rename(tempPath, path);
-    } finally {
-        unlock();
-    }
-};
 
 // --- Plex API Functions (Server-side only) ---
 const apiFetch = (url, token, options = {}) => {
